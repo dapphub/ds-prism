d8a31a8f805d40ef62b14ab1376dd229c7644484
Ryan Casey <ryepdx@gmail.com>
2017-08-25 06:20:14 -0700

    Bump solc version.

--- src/prism.sol
+++ src/prism.sol
@@ -13,7 +13,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-pragma solidity^0.4.13;
+pragma solidity^0.4.15;
 
 import 'ds-token/token.sol';
 import 'ds-thing/thing.sol';
